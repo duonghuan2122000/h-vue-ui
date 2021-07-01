512bdb9e94a673ec602b44b8b9ec1cb519547905
HUAN DUONG BANG <duong.huan2122000@gmail.com>
2021-07-01 10:46:35 +0700

    feat: xây dựng tooltip component
    xây dựng component tooltip cơ bản

---
+++ src/views/docs/tooltip/Tooltip.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h-tooltip text="tooltip text">Tooltip</h-tooltip>
+  </div>
+</template>
+
+<script>
+import HTooltip from "@/components/hTooltip/HTooltip.vue";
+export default {
+  components: {
+    HTooltip,
+  },
+};
+</script>
