67dc4d0b36cece3dadeda993728a1100185195f6
HUAN DUONG BANG <duong.huan2122000@gmail.com>
2021-07-02 11:53:53 +0700

    feat: Xây dựng checkbox radio
    thêm docs cơ bản

---
+++ src/views/docs/checkbox/Checkbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h-checkbox label="Checkbox" />
+  </div>
+</template>
+
+<script>
+import HCheckbox from "@/components/hCheckbox/HCheckbox.vue";
+export default {
+  components: {
+    HCheckbox,
+  },
+};
+</script>
