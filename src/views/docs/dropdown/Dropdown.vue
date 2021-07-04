16ac9965688ed607162029b07dd28ef7c06fdb7e
Jocelyn <duong.huan2122000@gmail.com>
2021-07-04 17:51:38 +0700

    feat: Xây dựng dropdown component
    Sửa lại docs dropdown component

--- src/views/docs/dropdown/Dropdown.vue
+++ src/views/docs/dropdown/Dropdown.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>
+  <div class="p-2">
     <h-dropdown :data="dropdownData">
-      <template slot="dropdown-toggle">
-        Dropdown
-      </template>
+      <template slot="dropdown-toggle"> Dropdown </template>
     </h-dropdown>
+
+    <pre class="my-1">
+&lt;h-dropdown :data="dropdownData"&gt;
+  &lt;template slot="dropdown-toggle"&gt; Dropdown &lt;/template&gt;
+&lt;/h-dropdown&gt;</pre>
+
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Tên prop</th>
+          <th>Kiểu dữ liệu</th>
+          <th>Giá trị mặc định</th>
+          <th>Mô tả</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>data</td>
+          <td>array</td>
+          <td></td>
+          <td>Data của dropdown content</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p>Slot</p>
+    <p>Dropdown có 2 slot</p>
+    <ul>
+      <li>slot: dropdown-toggle - Dropdown toggle thường là button (bắt buộc)</li>
+      <li>slot: dropdown-item - Dùng để custom lại item trong dropdown content (optional)</li>
+    </ul>
   </div>
 </template>
 
