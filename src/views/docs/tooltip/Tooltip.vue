c1351329242e1fdcef7e7ff8c3d5cd81016c4ee9
Jocelyn <duong.huan2122000@gmail.com>
2021-07-04 17:55:39 +0700

    feat: Xây dựng tooltip component
    Sửa docs

--- src/views/docs/tooltip/Tooltip.vue
+++ src/views/docs/tooltip/Tooltip.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
+  <div class="p-2">
     <h-tooltip text="tooltip text">Tooltip</h-tooltip>
+
+    <pre class="my-1">&lt;h-tooltip text="tooltip text"&gt;Tooltip&lt;/h-tooltip&gt;</pre>
+
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Tên prop</th>
+          <th>Kiểu dữ liệu</th>
+          <th>Giá trị mặc định</th>
+          <th>Mô tả</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>text</td>
+          <td>string</td>
+          <td>null</td>
+          <td>Text của tooltip</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
